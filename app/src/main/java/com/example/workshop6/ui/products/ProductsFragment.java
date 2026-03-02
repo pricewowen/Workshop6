d5e9986893dc6765699d90c8c66a3fe2c1df2691
masongga <auckmason@gmail.com>
2026-03-01 17:24:04 -0700

    added styling and layout to products fragment

--- app/src/main/java/com/example/workshop6/ui/products/ProductsFragment.java
+++ app/src/main/java/com/example/workshop6/ui/products/ProductsFragment.java
@@ -3,6 +3,8 @@ package com.example.workshop6.ui.products;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +18,9 @@ import com.example.workshop6.R;
  * create an instance of this fragment.
  */
 public class ProductsFragment extends Fragment {
+
+    // display rvCategories horizontally
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
